a5670bd5887b7c8afb2f4f4ff513d74a34c04d8b
SamiAibeche <samiaibeche@gmail.com>
2024-06-10 10:57:01 +0200

    File handling

--- File-handling/files.py
+++ File-handling/files.py
@@ -1,85 +1,42 @@
-import re
+import os
 
-#1. Create a regex that finds integers without size limit.
-s = "sss9gd7ds8sfsfs"
-pattern = r'\d+'
-result = re.findall(pattern, s)
-print(result)
+filename = "./data/data.txt"
+my_file = open(filename, "r")  # r for "read"
+# Can you put the contents of this file in the form of a list in which each element is a sentence ? (Use .split() for example...)
+content = my_file.read().split(' ')
+#print(content)
+my_file.close()
 
-#2. Create a regex that finds negative integers without size limit.
-s = "sssgdds-8sfs-9fs"
-pattern = r'-\d+'
-result = re.findall(pattern, s)
-print(result)
 
-#3. Create a regex that finds (positive or negative) integers without size limit.
-s = "sssgdds-8s8f9sfs"
-pattern = r'-?\d+'
-result = re.findall(pattern, s)
-print(result)
+# Get Path Name
+path = os.path.abspath("./data")
 
-#4. Capture all the numbers of the following sentence :
-text = "21 scouts and 3 tanks fought against 4,003 protestors, so the manager was not 100.00% happy."
-pattern = r'\b\d{1,3}(?:,\d{3})*(?:\.\d+)?\b'
-result = re.findall(pattern, text)
-print(result)
+#print(filename)
+#print(path)
+#print(type(path))
 
-#5. Find all words that end with 'ly'. :
-text = "He had prudently disguised himself but was quickly captured by the police."
-pattern = r'\b\w+ly\b'
-result = re.findall(pattern, text)
-print(result)
+# Manage Specific file under specified path
+filename = path+"/data.txt"
+my_file = open(filename, "w")  # w for "write"
+my_file.close()
 
-#6. License plate number :
+# Get all files from specified path
+os.path.basename(path)
 
-# A license plate consists of 2 capital letters, a dash ('-'), 3 digits, a dash ('-')
-# and finally 2 capital letters. Write a script to check that an input string is a license plate.#
-# If it's correct, print "good". If it's not correct, print "Not good".
-plate = input("Enter your license plate number: ")
-pattern = r'\b[A-Z]{2}-\d{3}-[A-Z]{2}\b'
-result = re.match(pattern, plate)
-if result is None:
-    print("Not good")
-else:
-    print("good")
+# Put all the .txt files from the data/ directory into a variable.
+# Then, copy the content of all the files from this variable into a file in data/ that you will name final.txt.
+for path, dirs, files in os.walk(path):
+    for filename in files:
+        if filename.endswith(".txt") and filename != "final.txt":
+            file_path = os.path.join(path, filename)
 
-#7 . Address IPV4
+            with open(file_path, 'rb') as f:
+                content = f.read().split()
 
-# An IPv4 address is composed of 4 numbers between 0 and 255 separated by '.'
-# Write a script to verify that a string entered is that of an IPv4 address.
-ip = input("Enter your IP address :")
-pattern = r'^(\d{1,3}\.){3}\d{1,3}$'
-result = re.match(pattern, ip)
-if result is None:
-    print("Not valid")
-else:
-    print("valid")
+            # Decode the content from bytes to strings, ignoring errors
+            # [expression for item in iterable]
+            content = [word.decode('utf-8', errors='ignore') for word in content]
 
-#8. Valid Mail
 
-# An email is composed of alphanumeric characters followed by @ and a domain name.
-# Write a script that checks that the string entered by a user is indeed that of an email,
-# otherwise ask him to re-enter it again (until he gets a valid email).
-mail = input("Enter a valid Mail :")
-pattern = r'^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]{1,3}$'
-result = re.match(pattern, mail)
-if result is None:
-    print("Not valid")
-else:
-    print("valid")
-
-#9. Valid Password
-
-# The password must now contain at least 6 characters AND
-# at least one lowercase letter AND
-# at least one uppercase letter AND
-# at least one number AND
-# at least one special character (among `$#@`).
-password = input("Enter your password :")
-pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$#@])[A-Za-z\d$#@]{6,}$'
-result = re.match(pattern, password)
-if result is None:
-    print("Not valid")
-else:
-    print("valid")
-
+            with open(os.path.join(path, "final.txt"), "a") as my_file:
+                my_file.write(' '.join(content) + '\n')
